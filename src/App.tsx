03d3f9b7b146c9ca59394054262a2eb08bb203d7
pi100000 <pinak001@protonmail.com>
2023-03-13 13:01:01 +0530

    changes3

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useEffect, useState } from "react";
-import CreateRecipe from "./components/CreateRecipe";
 import DisplayRecipes from "./components/DisplayRecipes";
 import RecipeForm from "./components/RecipeForm";
-import Recipes from "./components/Recipes";
 
 function App() {
   return (
     <>
       <DisplayRecipes />
       <RecipeForm />
-      {/* <CreateRecipe /> <Recipes /> */}
     </>
   );
 }
--- src/lib/pocketbase.js
+++
@@ -1,5 +0,0 @@
-import PocketBase from 'pocketbase';
-
-const pb = new PocketBase(process.env.REACT_PB_URL);
-
-export default pb;
